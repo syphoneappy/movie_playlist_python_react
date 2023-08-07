ed950d13c192fa72b47e6b2747b2331d20d532d2
SymphonyMLoom <harshtripathi669@gmail.com>
2023-08-07 19:57:08 +0530

    all

--- movie_list/views.py
+++ movie_list/views.py
@@ -14,7 +14,11 @@ from django.shortcuts import get_object_or_404
 from django.views.generic import TemplateView
 from django.views.decorators.cache import never_cache
 
-index = never_cache(TemplateView.as_view(template_name="index.html"))
+# index = never_cache(TemplateView.as_view(template_name="index.html"))
+
+
+def index(request):
+    return render(request, "index.html")
 
 
 @api_view(["GET", "POST"])
