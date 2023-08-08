7f77addd8598916f18be7c6b0fec753dae9ef808
SymphonyMLoom <harshtripathi669@gmail.com>
2023-08-08 07:48:33 +0530

    all

--- movie-playlist-app/src/components/AxiosInstance.jsx
+++ movie-playlist-app/src/components/AxiosInstance.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const Instance = axios.create({
-  baseURL: 'http://127.0.0.1:8000', // Replace with your Django backend URL
+  baseURL: 'https://shark-app-teuhd.ondigitalocean.app/', // Replace with your Django backend URL
   headers: {
     'Content-Type': 'application/json',
   },
