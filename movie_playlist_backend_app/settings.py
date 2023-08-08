13186adf997c514c72bc50159641cc9df9c5e948
SymphonyMLoom <harshtripathi669@gmail.com>
2023-08-08 07:42:11 +0530

    alls

--- movie_playlist_backend_app/settings.py
+++ movie_playlist_backend_app/settings.py
@@ -30,8 +30,8 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 DEBUG = os.getenv("DEBUG", "False") == "True"
-# ALLOWED_HOSTS = ["*"]
-ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1, localhost").split(",")
+ALLOWED_HOSTS = ["*"]
+# ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1, localhost").split(",")
 
 CORS_ALLOW_ALL_ORIGINS = True
 
